4ac120a2454843af191aed05dde121bb55b38103
Kenny Yuan <kaining.yuan@intel.com>
2017-02-23 16:33:26 +0800

    Remove unused files

--- src/idl/common/enable-eventemitter.js
+++
@@ -1,9 +0,0 @@
-////////////////////////////////////////////////////////////
-var EventEmitter = require('events').EventEmitter;
-function inherits(target, source) {
-  for (var k in source.prototype) {
-    target.prototype[k] = source.prototype[k];
-  }
-}
-
-inherits(Instance, EventEmitter);
